12c50ea2bba5b591a53d4e5e17a80078d0ef1f12
abeedalayyubisalim <abeedalayyubisalim@gmail.com>
2026-08-30 11:05:32 +0800

    Delete fishing-fps directory

--- fishing-fps/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-// Relative base so the built site works when served from a GitHub Pages
-// project path (https://<user>.github.io/<repo>/) without extra config.
-export default defineConfig({
-  base: './',
-})
